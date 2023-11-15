93a460009fbc7e533b3569fb8752c4968d0a2fb6
Annisah Husni Daulay <138286798+bandelkid@users.noreply.github.com>
2023-11-15 16:30:19 +0700

    Create login.php

---
+++ login.php
@@ -0,0 +1,86 @@
+<?php
+require_once "config.php";
+
+// Inisialisasi variabel
+$username = $password = "";
+$username_err = $password_err = "";
+
+// Proses data formulir saat tombol login ditekan
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Validasi username
+    if (empty(trim($_POST["username"]))) {
+        $username_err = "Please enter your username.";
+    } else {
+        $username = trim($_POST["username"]);
+    }
+
+    // Validasi password
+    if (empty(trim($_POST["password"]))) {
+        $password_err = "Please enter your password.";
+    } else {
+        $password = trim($_POST["password"]);
+    }
+
+    // Jika tidak ada kesalahan validasi
+    if (empty($username_err) && empty($password_err)) {
+        // Query untuk mencari pengguna dengan username yang sesuai
+        $query = "SELECT * FROM users WHERE username='$username'";
+        $result = mysqli_query($link, $query);
+
+        // Periksa apakah pengguna ditemukan
+        if (mysqli_num_rows($result) > 0) {
+            $row = mysqli_fetch_assoc($result);
+            // Verifikasi password
+            if (password_verify($password, $row['password'])) {
+                // Password cocok, proses login
+                echo "Login berhasil!";
+                header("location:home.php");
+            } else {
+                // Password tidak cocok, tampilkan pesan error
+                $password_err = "Invalid password. Please try again.";
+            }
+        } else {
+            // Pengguna tidak ditemukan, tampilkan pesan error
+            $username_err = "No account found with that username.";
+        }
+    }
+}
+
+// Tutup koneksi
+mysqli_close($link);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 350px; padding: 20px; }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <h2>Login</h2>
+        <p>Please fill in your credentials to login.</p>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+                <label>Username</label>
+                <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
+                <span class="help-block"><?php echo $username_err; ?></span>
+            </div>    
+            <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+                <label>Password</label>
+                <input type="password" name="password" class="form-control">
+                <span class="help-block"><?php echo $password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Login">
+            </div>
+            <p>Don't have an account? <a href="register.php">Sign up now</a>.</p>
+        </form>
+    </div>    
+</body>
+</html>
